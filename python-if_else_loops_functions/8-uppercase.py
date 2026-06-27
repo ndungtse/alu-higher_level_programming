6a8a43538444c973839add148a17a389165b9bdc
ndungtse <ndungutsecharles103@gmail.com>
2026-06-27 21:27:09 +0200

    Add python-if_else_loops_functions task scripts

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            print(chr(ord(c) - 32), end="")
+        else:
+            print(c, end="")
+    print("")
