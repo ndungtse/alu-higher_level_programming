34234d758127b263a1fbec90fe85ef863036e450
ndungtse <ndungutsecharles103@gmail.com>
2026-06-27 21:14:16 +0200

    Refactor string concatenation in concat scripts for improved clarity and formatting

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School!"
-new_str = str1 + " " + str2
+str1 = "Holberton"
+str2 = "School"
+new_str = f"Welcome to {str1} {str2}!"
 print(new_str)
